b9198de9df3c1bee308b70d1007dd68f6b1e72fb
carotesel <100797700+carotesel@users.noreply.github.com>
2022-06-29 11:59:43 -0300

    jwt empezado

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 import express from 'express'
 import peliculaController from './src/controllers/peliculaController.js'
 import personajeController from './src/controllers/personajeController.js'
+import jsonwebtoken from 'jsonwebtoken';
 
 const app = express();
 
 const port = 3001;
+
+const jwt = jsonwebtoken();
 
 app.set('port', port);
 app.listen(app.get('port'));
@@ -12,6 +15,9 @@ app.listen(app.get('port'));
 console.log("server en el puerto: ",app.get('port'));
 
 app.use(express.json());
+
+
+
 app.use("/movies", peliculaController);
 app.use("/characters", personajeController);
 // app.use(express.urlencoded({ extended: false }));
